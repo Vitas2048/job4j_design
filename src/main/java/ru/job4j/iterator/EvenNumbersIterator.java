cce64d2e15f72e5520399809bacb9cd2b6d33b3b
Vitas2048 <vithag97@mail.ru>
2022-06-14 22:46:50 +0700

    5.1.2. Создать итератор четные числа [#150]

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -28,9 +28,8 @@ public class EvenNumbersIterator implements Iterator<Integer> {
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
-        } else {
-            return data[index++];
         }
+        return data[index++];
     }
 
 }
