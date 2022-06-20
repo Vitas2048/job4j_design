a08b79a9d2fb7215e3f5d6c3b9266fde875a4536
Vitas2048 <vithag97@mail.ru>
2022-06-20 23:21:39 +0700

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generic/MemStore.java
@@ -0,0 +1,35 @@
+package ru.job4j.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final Map<String, T> storage = new HashMap<>();
+
+    @Override
+    public void add(T model) {
+        storage.putIfAbsent(model.getId(), model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+
+        return storage.replace(id, model) != null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return storage.remove(id, storage.get(id));
+    }
+
+    @Override
+    public T findById(String id) {
+        try {
+            storage.get(id);
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException");
+        }
+        return storage.get(id);
+    }
+}
