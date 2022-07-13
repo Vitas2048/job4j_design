d1f6463b17fd307bf8e5001684987d6790170723
Vitas2048 <vithag97@mail.ru>
2022-07-13 21:54:50 +0700

    7. ListIterator [#350217 #321096]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,21 +7,7 @@ import java.util.function.Function;
 
 public class ListUtils {
 
-    public static boolean check(Predicate<ListIterator> s, ListIterator eiterator) {
-        return s.test(eiterator);
-    }
 
-    public static boolean hnext(ListIterator e) {
-        return check(ListIterator::hasNext, e);
-    }
-
-    public static boolean fil(Predicate p, ListIterator e) {
-        return check(s -> p.test(s.next()), e);
-    }
-
-    public static boolean eq(ListIterator e, ListIterator f) {
-        return check(s -> s.next().equals(e.next()), f);
-    }
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
@@ -31,15 +17,14 @@ public class ListUtils {
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(index);
-        iterator.next();
+        ListIterator<T> iterator = list.listIterator(index + 1);
         iterator.add(value);
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> iterator = list.listIterator();
-        while (hnext(iterator)) {
-            if (fil(filter, iterator)) {
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
                 iterator.set(value);
             }
         }
@@ -47,20 +32,14 @@ public class ListUtils {
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> iterator = list.listIterator();
-        while (hnext(iterator)) {
-            if (fil(filter, iterator)) {
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
                 iterator.remove();
             }
         }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> iterator = list.listIterator();
-        ListIterator<T> eIterator = elements.listIterator();
-        while (hnext(iterator) && hnext(eIterator)) {
-            if (eq(iterator, eIterator)) {
-                iterator.remove();
-            }
-        }
+        removeIf(list, elements::contains);
     }
 }
