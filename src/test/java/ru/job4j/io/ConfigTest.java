3d843eb8ed3e47969706e12eca08e7acb4beae4e
Vitas2048 <vithag97@mail.ru>
2022-09-21 01:06:35 +0700

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ConfigTest {
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/test.txt";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Vitaly");
+    }
+
+    @Test
+     void whenPairWitComment() throws UnsupportedOperationException {
+
+        String path = "./data/test.txt";
+        Config config = new Config(path);
+        config.load();
+        UnsupportedOperationException thrown = assertThrows(
+                UnsupportedOperationException.class,
+                () -> config.value("name3"),
+                "Don't impl this method yet!"
+        );
+        assertTrue(thrown.getMessage().contains("Don't impl this method yet!"));
+        assertThat(config.value("name1")).isEqualTo("Vitaly");
+        assertThat(config.value("name2")).isEqualTo("Alex");
+    }
+}
