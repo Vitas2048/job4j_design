505ad2b095a3c8c29d2cc0dda5903d42b996abbc
Vitas2048 <vithag97@mail.ru>
2022-10-06 15:44:04 +0700

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,27 +1,84 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
-        String sep = argsName.get("-delimiter");
+        validation(argsName);
+        String sep = argsName.get("delimiter");
+        String path = argsName.get("out");
         ArrayList<String> filter = new ArrayList<>();
-        var scanner = new Scanner(argsName.get("-filter")).useDelimiter(",");
+        ArrayList<String> res = new ArrayList<>();
+        HashMap<String, Integer> col = new HashMap<>();
+        var scanner = new Scanner(argsName.get("filter")).useDelimiter(",");
         while (scanner.hasNext()) {
             filter.add(scanner.next());
         }
         try {
-            BufferedReader in = new BufferedReader(new FileReader(argsName.get("-path")));
-            in.lines().forEach(System.out::println);
+            BufferedReader in = new BufferedReader(new FileReader(argsName.get("path")));
+            String line;
+            while ((line = in.readLine()) != null) {
+                int i = 0;
+                String string = "";
+                for (String s:filter) {
+                var scanner1 = new Scanner(line).useDelimiter(sep);
+                while (scanner1.hasNext()) {
+                    i++;
+                    String scan = scanner1.next();
+                        if (scan.equals(s)) {
+                            col.put(s, i);
+                        }
+                        if (col.get(s) != null && i == col.get(s)) {
+                            string = String.format("%s%s%s", string, scan, sep);
+                        }
+                    }
+                }
+                string = string.substring(0, string.length() - 1);
+                res.add(string);
+            }
+            try (FileOutputStream out = new FileOutputStream(path)) {
+                for (String s: res) {
+                    if (path.equals("stdout")) {
+                        System.out.println(s);
+                    } else {
+                        out.write(s.getBytes());
+                        out.write(System.lineSeparator().getBytes());
+                    }
+                }
+                }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void validation(String[] n) {
+    public static void validation(ArgsName argsName) {
+        if (argsName.get("delimiter").length() != 1) {
+            throw new IllegalArgumentException(
+                    String.format("необходимо ввести разделитель в строке %s", argsName.get("delimiter")));
+        }
+        if (!argsName.get("out").equals("stdout") && !(argsName.get("out")).endsWith(".csv")) {
+            throw new IllegalArgumentException(
+                    String.format("необходимо ввести stdout для вывода на консоль или путь к файлу в строке %s",
+                            argsName.get("out")));
+        }
+        if (argsName.get("filter").contains(", ")) {
+            throw new IllegalArgumentException(
+                    String.format("необходимо ввести фильтры строго через запятую %s", argsName.get("filter")));
+        }
+        if (!Paths.get(argsName.get("path")).toFile().isFile() && !argsName.get("path").endsWith(".csv")) {
+            throw new IllegalArgumentException(
+                    String.format("необходимо ввести путь к файлу формата csv %s", argsName.get("path")));
+        }
+    }
 
+    public static void main(String[] args) throws Exception {
+        ArgsName name = ArgsName.of(args);
+        handle(name);
     }
 }
