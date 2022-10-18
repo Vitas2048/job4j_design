46a56e1e6e43b35a856503b3f3312dc620ec4b8e
Vitas2048 <vithag97@mail.ru>
2022-10-18 16:21:36 +0700

    0. Что такое база данных. [#6860]

---
+++ Connection.sql
@@ -0,0 +1,5 @@
+insert into monitors(names, gaming, frequency) values('Asus', true, 144);
+update monitors set names = 'Samsung';
+select * from monitors;
+delete from monitors;
+select * from monitors;
