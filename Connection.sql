2b190bff43cb138242d362f93287f7d58ca673b4
Vitas2048 <vithag97@mail.ru>
2022-10-18 20:22:33 +0700

    0. Что такое база данных. [#6860 #352835]

--- Connection.sql
+++ Connection.sql
@@ -1,8 +1,9 @@
 create table monitors(
+    id serial primary key,
     names varchar(255),
     gaming boolean,
     frequency integer
-)
+);
 insert into monitors(names, gaming, frequency) values('Asus', true, 144);
 update monitors set names = 'Samsung';
 delete from monitors;
