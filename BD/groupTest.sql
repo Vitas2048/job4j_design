b39e1fe33652921b48aff897aa6a691bc1a83c62
Vitas2048 <vithag97@mail.ru>
2022-10-20 18:08:58 +0700

    2. Группировка и агрегатные функции [#373618 #353506]

--- BD/groupTest.sql
+++ BD/groupTest.sql
@@ -38,5 +38,16 @@ insert into devices_people(device_id, people_id) values(3, 4);
 insert into devices_people(device_id, people_id) values(4, 4);
 
 select avg(price) from devices;
-select p.name, avg(d.price) from devices_people dp join people p on dp.people_id = p.id join devices d on dp.device_id = d.id group by p.name;
-select p.name, avg(d.price) from devices_people dp join people p on dp.people_id = p.id join devices d on dp.device_id = d.id group by p.name having avg(d.price) > 5000;
+select p.name, avg(d.price) from devices_people dp 
+join people p 
+on dp.people_id = p.id 
+join devices d 
+on dp.device_id = d.id 
+group by p.name;
+
+select p.name, avg(d.price) from devices_people dp 
+join people p 
+on dp.people_id = p.id 
+join devices d 
+on dp.device_id = d.id 
+group by p.name having avg(d.price) > 5000;
