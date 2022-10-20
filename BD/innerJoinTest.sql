42b71f73bbe373cbbd75aaaf05291b03b5d90251
Vitas2048 <vithag97@mail.ru>
2022-10-20 15:45:04 +0700

    1. Inner join [#373617]

--- BD/innerJoinTest.sql
+++ BD/innerJoinTest.sql
@@ -1,22 +1,24 @@
-create table pet(
-	id serial primary key,
-	breed varchar(255),
-	name varchar(255)
-);
 create table owner(
 	id serial primary key,
 	name varchar(255),
-	pet_id int references pet(id),
 	apparts boolean
 );
 
-insert into pet(breed, name) values('dog', 'Reks');
-insert into pet(breed, name) values('cat', 'Sam');
+create table pet(
+	id serial primary key,
+	breed varchar(255),
+	name varchar(255),
+	owner_id int references owner(id)
+);
 
-insert into owner(name, pet_id, apparts) values ('Natasha', 1, true);
-insert into owner (name, pet_id, apparts) values ('Pete', 2, false);
-insert into owner(name, pet_id, apparts) values('Mike', null, true);
 
-select p.breed, o.name from pet p join owner o on o.pet_id = p.id; 
-select p.name, o.name from pet p join owner o on o.pet_id = p.id; 
-select p.breed, o.apparts from pet p join owner o on o.pet_id = p.id; 
+insert into owner(name, apparts) values ('Natasha', true);
+insert into owner (name, apparts) values ('Pete', false);
+insert into owner(name, apparts) values('Mike', true);
+
+insert into pet(breed, name, owner_id) values('dog', 'Reks', 1);
+insert into pet(breed, name, owner_id) values('cat', 'Sam', 2);
+
+select p.breed, o.name from pet p join owner o on p.owner_id = o.id; 
+select p.name, o.name from pet p join owner o on p.owner_id = o.id; 
+select p.breed, o.apparts from pet p join owner o on p.owner_id = o.id; 
