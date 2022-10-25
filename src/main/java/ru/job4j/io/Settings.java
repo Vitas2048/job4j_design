66b86803f21b9df2c49c918fed1888e827f4f30f
Vitas2048 <vithag97@mail.ru>
2022-10-25 14:42:16 +0700

    0. JDBC [#6863]

--- src/main/java/ru/job4j/io/Settings.java
+++ src/main/java/ru/job4j/io/Settings.java
@@ -1,29 +1,12 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Settings {
-
-    private final Properties prs = new Properties();
-
-    public void load(InputStream io) {
-        try {
-            this.prs.load(io);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getValue(String key) {
-        return this.prs.getProperty(key);
-    }
-
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.postgresql.Driver");
         Config config = new Config("./../app.properties");
