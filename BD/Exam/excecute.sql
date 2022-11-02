4d68a54a39a6f99d9af213a10c2dff9188fe3485
Vitas2048 <vithag97@mail.ru>
2022-11-02 15:49:50 +0700

    2. SQL Select by company [#2760]

--- BD/Exam/excecute.sql
+++ BD/Exam/excecute.sql
@@ -3,24 +3,15 @@ from person p join company c
 on p.company_id = c.id and not c.id = 5;
 
 
-select c.name, max(v.count)
-from
-(select c.name, count(*)
-from
-(select p.company_id, c.id
-from company c join person p
-on p.company_id = c.id) v join company c
-on v.company_id = c.id
-group by c.name) v join company c
-on c.name = v.name
-where v.count = (
-	select max(v.count) 
-		from (
-	select c.name, count(*)
-			from
-				(select p.company_id, c.id
-					from company c join person p
-					on p.company_id = c.id) v join company c
-					on v.company_id = c.id
-					group by c.name) v)
-group by c.name;
+select c.name,
+count(p.company_id) persons
+from company c
+join person p
+on c.id = p.company_id
+GROUP by c.name
+having count(p.company_id)=(
+	select count(p.company_id)
+	from person p 
+	group by p.company_id
+	order by count(p.company_id) desc
+	limit 1);
