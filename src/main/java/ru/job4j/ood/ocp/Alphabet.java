bb72fd642a76b3e3073b7d2a55b5d613f3a6bf53
Vitas2048 <vithag97@mail.ru>
2022-11-23 18:51:08 +0700

    0. Принцип открытости закрытости [#4914 #363158]

--- src/main/java/ru/job4j/ood/ocp/Alphabet.java
+++ src/main/java/ru/job4j/ood/ocp/Alphabet.java
@@ -2,11 +2,15 @@ package ru.job4j.ood.ocp;
 
 public class Alphabet {
     public void check(AbstractLet letter) {
+        int s;
         if (letter instanceof A) {
+            s = 1;
         }
         if (letter instanceof B) {
+            s = 2;
         }
         if (letter instanceof C) {
+            s = 3;
         }
     }
 }
